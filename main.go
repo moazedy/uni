c8c807013325b1de22f530873599d72f104975d0
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-08-14 13:13:49 -0400

    intracting with system in the most simple way in  main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"uni/repository"
+	"uni/utils"
+)
+
+func main() {
+	repository.Init()
+
+	utilsInterface := utils.NewCuorseInterfaceUtils()
+	idPointer, err := utilsInterface.CreateCuorse("sp")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("your new cuorse id is : ", *idPointer)
+
+	dataPointer, err := utilsInterface.GetCuorseData(*idPointer)
+	if err != nil {
+		fmt.Println("error : ", err)
+		return
+	}
+
+	fmt.Println("your requested cuorse data: ", *dataPointer)
+
+}

a41f6dc74baa46b952b60251d8e2d3fb300d0ac1
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-08-14 13:13:04 -0400

    implementing utils layer as upper and logical  layer of system

---
+++ utils/cuorse.go
@@ -0,0 +1,70 @@
+package utils
+
+import (
+	"errors"
+	"uni/dataModels"
+	"uni/repository"
+
+	"github.com/google/uuid"
+)
+
+// CuorseInterface an interface for connect with upper layers
+type CuorseInterfaceUtils interface {
+	// CreateCurse addes a new cuorse to db
+	CreateCuorse(name string) (*string, error)
+	// GetCuorseData returns data of target Course
+	GetCuorseData(cuorseId string) (*dataModels.Cuorse, error)
+}
+
+// cuorse is a struct to hold interface methods and connect to lower layers
+type cuorse struct {
+	repo repository.CuorseDbInterface
+}
+
+// NewCuorseInterface is constructory func for creating an instance of CuorseInterface
+func NewCuorseInterfaceUtils() CuorseInterfaceUtils {
+	var CuorseInterface CuorseInterfaceUtils
+
+	c := &cuorse{
+		repo: repository.NewCuorsesDbInterface(),
+	}
+
+	CuorseInterface = c
+
+	return CuorseInterface
+}
+
+func (c *cuorse) CreateCuorse(name string) (*string, error) {
+
+	newCuorse := dataModels.Cuorse{
+		Id:   uuid.New().String(),
+		Name: name,
+	}
+
+	err := newCuorse.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := c.repo.WriteNewCuorse(newCuorse)
+	if err != nil {
+		return nil, err
+	}
+
+	return id, nil
+}
+
+func (c *cuorse) GetCuorseData(cuorseId string) (*dataModels.Cuorse, error) {
+
+	_, err := uuid.Parse(cuorseId)
+	if err != nil {
+		return nil, errors.New("invalid id")
+	}
+
+	cuorseData, err := c.repo.ReadCuorse(cuorseId)
+	if err != nil {
+		return nil, err
+	}
+
+	return cuorseData, nil
+}

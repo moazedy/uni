e03bf0c4df37be8032fa6633c5fd2f84a3a855cb
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-08-23 17:59:36 -0400

    calling on controller.Run function

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"uni/controller"
 	"uni/repository"
-	"uni/utils"
 )
 
 func main() {
 	repository.Init()
 
-	utilsInterface := utils.NewCuorseInterfaceUtils()
-	idPointer, err := utilsInterface.CreateCuorse("sp")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("your new cuorse id is : ", *idPointer)
-
-	dataPointer, err := utilsInterface.GetCuorseData(*idPointer)
-	if err != nil {
-		fmt.Println("error : ", err)
-		return
-	}
-
-
-	fmt.Println("your requested cuorse data: ", *dataPointer)
-
+	controller.Run(":2727")
 }
